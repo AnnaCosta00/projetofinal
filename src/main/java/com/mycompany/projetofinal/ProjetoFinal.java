c739cd6f7dbd1a26ea97e5676d3655556d7f32ac
AnnaCosta <anna_beatriz00@outlook.com>
2024-11-13 10:22:47 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/projetofinal/ProjetoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofinal;
+
+/**
+ *
+ * @author Luan Santos
+ */
+public class ProjetoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
